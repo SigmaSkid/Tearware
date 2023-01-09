553f823cf96cbd5b14f6ea45f2f4a74b5872443d
SigmaSkid <informkilioo@gmail.com>
2023-01-09 10:24:55 +0100

    m

--- main.lua
+++ main.lua
@@ -1,12 +1,6 @@
 -- I hate lua, let's compile C++ into lua.
 
-#include "source/globals.lua"
-#include "source/config.lua"
-#include "source/utils.lua"
-
-#include "source/update.lua"
-#include "source/tick.lua"
-#include "source/draw.lua"
+#include "source/main.lua"
 
 -- imagine doing code in the main file!
 -- that's just ridiculous
---
+++ source/main.lua
@@ -0,0 +1,10 @@
+-- I hate lua, let's compile C++ into lua.
+
+#include "globals.lua"
+#include "config.lua"
+#include "utils.lua"
+#include "update.lua"
+#include "tick.lua"
+#include "draw.lua"
+
+-- still no code here. 
