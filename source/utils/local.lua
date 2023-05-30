8dc1657d06228eef33c7d430579d6aaa9d525e24
Sigma <51486191+SigmaSkid@users.noreply.github.com>
2023-05-31 01:40:12 +0200

    Update local.lua

--- source/utils/local.lua
+++ source/utils/local.lua
@@ -3,46 +3,46 @@
 -- obviously change it before uploading to workshop.
 fProjectName = "Tearware Github Preview"
 
--- fFeature = {"menu name", "config string"}
+-- fFeature = {"menu name", "config string", "explanation"}
 -- visuals
 fVisuals = {"Visuals", "visuals"}
 fWatermark = {"Watermark", "watermark"}
-fFeatureList = {"Feature List", "featurelist"}
-fObjectiveEsp = {"Objective ESP", "objectiveesp"}
-fOptionalEsp = {"Optional ESP", "optionalesp"}
-fValuableEsp = {"Valuable ESP", "valueesp"}
-fToolEsp = {"Tool ESP", "toolesp"}
-fWeaponGlow = {"Weapon Glow", "weaponglow"}
-fActiveGlow = {"Active Glow", "activeglow"}
-fRainbowFog = {"Colored Fog", "rainbowfog"}
+fFeatureList = {"Feature List", "featurelist", "Lists enabled features"}
+fObjectiveEsp = {"Objective ESP", "objectiveesp", "Highlights mandatory objectives"}
+fOptionalEsp = {"Optional ESP", "optionalesp", "Highlights optional objectives"}
+fValuableEsp = {"Valuable ESP", "valueesp", "Highlights valuables"}
+fToolEsp = {"Tool ESP", "toolesp", "Highlights tools"}
+fWeaponGlow = {"Weapon Glow", "weaponglow", "Highlights held tool"}
+fActiveGlow = {"Active Glow", "activeglow", "Highlights active bodies"}
+fRainbowFog = {"Colored Fog", "rainbowfog", "Changes the color of fog"}
 fPostProcess = {"Post Processing", "postprocessing"}
-fSpinnyTool = {"Spinny Tool", "spinnytool"}
+fSpinnyTool = {"Spinny Tool", "spinnytool", "Makes your tool spin"}
 
 -- player
-fSpeed = {"Speed", "speedhack"}
-fSpider = {"Spider", "spider"}
-fFly = {"Fly", "fly"}
-fNoclip = {"Noclip", "noclip"}
-fFloorStrafe = {"Floor Strafe", "floorstrafe"}
+fSpeed = {"Speed", "speedhack", "Makes you faster"}
+fSpider = {"Spider", "spider", "Allows you to climb walls"}
+fFly = {"Fly", "fly", "Allows you to fly"}
+fNoclip = {"Noclip", "noclip", "Allows you to noclip"}
+fFloorStrafe = {"Floor Strafe", "floorstrafe", "Disables ground friction"}
 fJetpack = {"Jetpack", "jetpack"}
-fJesus = {"Jesus", "jesus"}
-fQuickstop = {"Quickstop", "quickstop"}
-fInfiniteAmmo = {"Infinite Ammo", "infiniteammo"}
-fSuperStrength = {"Super Strength", "superstrength" }
-fGodmode = {"Godmode", "godmode"}
-fRubberband = {"Rubberband", "rubberband"}
+fJesus = {"Jesus", "jesus", "Let's you turn water into wine"}
+fQuickstop = {"Quickstop", "quickstop", "Stops your velocity when there is no input"}
+fInfiniteAmmo = {"Infinite Ammo", "infiniteammo", "Disables ammo limit"}
+fSuperStrength = {"Super Strength", "superstrength", "Allows you to grab all dynamic objects"}
+fGodmode = {"Godmode", "godmode", "Prevents health loss"}
+fRubberband = {"Rubberband", "rubberband", "Teleports you to your previous position"}
 
 -- world
 fBulletTime = {"Slowmotion", "timer"}
-fBulletTimeScale = {"Scale", "scale"}
-fBulletTimePatch = {"Patch", "patch"}
-fSkipObjective = {"Skip Objective", "skipobjective"}
-fDisableAlarm = {"Disable Alarm", "disablealarm"}
-fDisableRobots = {"Disable Robots", "disablerobots"}
-fDisablePhysics = {"Disable Physics", "disablephysics"}
-fForceUpdatePhysics = {"Force Update Physics", "forceupdatephysics"}
-fTeleportValuables = {"Teleport Valuables", "autocollect"}
-fUnfairValuables = {"Unfair Valuables", "inflation"}
+  fBulletTimeScale = {"Scale", "scale"}
+  fBulletTimePatch = {"Patch", "patch"}
+fSkipObjective = {"Skip Objective", "skipobjective", "Marks all objectives as completed"}
+fDisableAlarm = {"Disable Alarm", "disablealarm", "Disables alarm"}
+fDisableRobots = {"Disable Robots", "disablerobots", "Disables robots"}
+fDisablePhysics = {"Disable Physics", "disablephysics", "Disables dynamic objects"}
+fForceUpdatePhysics = {"Force Update Physics", "forceupdatephysics", "Updates all dynamic objects"}
+fTeleportValuables = {"Teleport Valuables", "autocollect", "Teleports all valuables to you"}
+fUnfairValuables = {"Unfair Valuables", "inflation", "Increases value of all valuables"}
 fStructureRestorer = {"Structure Restorer", "structurerestorer"}
 
 -- tools
