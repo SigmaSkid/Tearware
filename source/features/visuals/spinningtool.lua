b07492c9ffd9ceee5f47ef7dd69fc4f40ff6e7f0
Sigma <51486191+SigmaSkid@users.noreply.github.com>
2023-07-10 15:29:55 +0200

    Update spinningtool.lua

--- source/features/visuals/spinningtool.lua
+++ source/features/visuals/spinningtool.lua
@@ -1,6 +1,11 @@
-spinny_tool_current_vector = {0,0,0}
-spinny_tool_target_vector = {0,0,0}
-spinny_tool_started_transition = 0
+spinny_tool_current_rot = {0,0,0}
+spinny_tool_target_rot = {0,0,0}
+
+spinny_tool_current_pos = {0,0,0}
+spinny_tool_target_pos = {0,0,0}
+
+spinny_tool_started_transition_rot = 0
+spinny_tool_started_transition_pos = 0
 
 visuals_SpinningTool = function()
     if not config_AdvGetBool(fSpinnyTool) then 
@@ -14,23 +19,39 @@ visuals_SpinningTool = function()
 
     local speed = config_GetSubFloat(fSpinnyTool, fSubSpeed)
 
-    local dist = utils_VecDist(spinny_tool_current_vector, spinny_tool_target_vector)
-    local transition_state = (GetTime() - spinny_tool_started_transition) * speed /dist
+    local dist_rot = utils_VecDist(spinny_tool_current_rot, spinny_tool_target_rot)
+    local dist_pos = utils_VecDist(spinny_tool_current_pos, spinny_tool_target_pos)
 
-    if transition_state >= 1 then 
-        spinny_tool_current_vector = spinny_tool_target_vector
-        spinny_tool_target_vector = utils_RandomVector()
-        spinny_tool_started_transition = GetTime()
-        transition_state = 0
+    local transition_state_rot = (GetTime() - spinny_tool_started_transition_rot) * speed /dist_rot
+    local transition_state_pos = (GetTime() - spinny_tool_started_transition_pos) * speed /dist_pos
+
+    -- infinite loop potential!
+    -- GetTime() should in theory prevent infinite loop... but yk.
+    -- :shrug:
+    if transition_state_rot >= 1 then 
+        spinny_tool_current_rot = spinny_tool_target_rot
+        spinny_tool_target_rot = utils_RandomVector()
+        spinny_tool_started_transition_rot = GetTime()
+        
         -- the power of infinite loops and recursion!
         visuals_SpinningTool()
-        return 
-    end
 
-    local transition = VecLerp(spinny_tool_current_vector, spinny_tool_target_vector, transition_state)
-    local rot = QuatEuler(transition[1] * 180, transition[2] * 180, transition[3] * 180)
+    elseif transition_state_pos >= 1 then 
+        spinny_tool_current_pos = spinny_tool_target_pos
+        spinny_tool_target_pos = utils_RandomVector()
+        spinny_tool_started_transition_pos = GetTime()
+
+        -- the power of infinite loops and recursion!
+        visuals_SpinningTool()
     
-    local offset = Transform( VecAdd( {-0.1, 0, -2}, VecScale(rot, -0.5)),  rot)
+    else 
+        local transition_rot = VecLerp(spinny_tool_current_rot, spinny_tool_target_rot, transition_state_rot)
+        local rot = QuatEuler(transition_rot[1] * 180, transition_rot[2] * 180, transition_rot[3] * 180)
+        
+        local pos = VecLerp(spinny_tool_current_pos, spinny_tool_target_pos, transition_state_pos)
 
-    SetToolTransform(offset)
+        local offset = Transform( VecAdd( {-0.5, 0, -2}, pos),  rot)
+
+        SetToolTransform(offset)
+    end
 end
